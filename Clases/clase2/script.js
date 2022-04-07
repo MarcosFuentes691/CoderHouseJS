3524434943f760d777ce80a64e7d4f02f0df1b9c
Marcos Fuentes <marcosfuentes691@gmail.com>
2022-04-07 18:36:26 -0300

    Clases 1 a 6

---
+++ Clases/clase2/script.js
@@ -0,0 +1,86 @@
+// // = asignacion
+// let edad = 18;
+
+// //para comparar -  == - operador logico
+// let mayorEdad = (edad == 18); //false
+
+// //IF
+
+// if (mayorEdad) {
+//     //codigo
+//     alert("sos mayor de edad!!");
+// }
+
+// let deporteFavorito = prompt("Cual es tu deporte favorito?");
+// deporteFavorito = deporteFavorito.toLowerCase(); // toUpperCase()
+
+// if ((deporteFavorito == "voley") || (deporteFavorito == "VOLEY") || (deporteFavorito == "Voley")) {
+//     alert("el mio tambien!");
+// } else if (deporteFavorito == "rugby") {
+//     alert("ese deporte no me gusta")
+// } else {
+//     //alert("Tu deporte favorito es " + deporteFavorito + ". el mio es el voley");
+//     //string template
+//     alert(`Tu deporte favorito es ${deporteFavorito}. el mio es el voley`);
+// }
+
+// let nota = parseInt(prompt("Cual fue tu nota en matematica?"));
+
+// if (nota <= 4) {
+//     alert("desaprobaste!");
+// } else if (nota <= 7) {
+//     alert("tenes que mejorar tu nota un poco");
+// } else {
+//     alert("te felicito");
+// }
+
+// let precio = parseInt(prompt("cual es el precio?"));
+
+// // si el precio es mayor a 1000 entonces, aplicamos un descuento del 50%
+// // si no es mayor a 1000 entonces, aplicamos un descuento del 25%
+// let precioNuevo;
+// if(precio > 1000) {
+//     precioNuevo = precio / 2;
+// } else {
+//     precioNuevo = precio * 0.75;
+// }
+// console.log(`el precio nuevo es ${precioNuevo}`);
+
+
+// let nota1 = parseInt(prompt("Cual es tu nota"));
+// let nota2 = parseInt(prompt("Cual es tu 2  nota"));
+// let nota3 = parseInt(prompt("Cual es tu 3  nota"));
+
+// let promedio = ((nota1 + nota2 + nota3)  / 3);
+
+// if(promedio > 6){
+//     alert ("genial aprobaste con mayor ");
+
+
+// } else if ( promedio == 6 )
+// {
+//     alert ("genial aprobaste ") ;
+// }
+// else
+// {
+//     alert ("desprobaste");
+// }
+
+
+// let num1 = 1;
+// let numUno = "1";
+
+// let sonIguales = (num1 === numUno);
+
+// console.log(sonIguales);
+
+
+//EJERCICIO
+/*
+Preguntar al usuario su edad
+
+Si tiene - entre 0 y 17 anios - dato de salida - "Eres un/a ninio/a"
+si tiene entre 18 y 40 - "sos un/a joven"
+si tiene mas  - " trajiste tu baston??"
+*/
+
